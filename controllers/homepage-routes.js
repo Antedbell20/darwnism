2e468cf09ffeb6ace4b2a5216790052c29e5c1a7
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-11-08 13:29:25 -0800

    buttons made to easily switch between pages during development

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -6,6 +6,46 @@ router.get('/', (req, res) => {
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
+
+router.get('/dashboard', (req, res) => {
+    try{
+        res.render('dashboard');
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+router.get('/finalScore', (req, res) => {
+    try{
+        res.render('finalScore');
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+router.get('/highScore', (req, res) => {
+    try{
+        res.render('highScore');
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+router.get('/login', (req, res) => {
+    try{
+        res.render('login');
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+router.get('/questionPage', (req, res) => {
+    try{
+        res.render('questionPage');
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
