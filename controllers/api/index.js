f7bf2f93ed4acf414b93896a2acab0982407e65b
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-11-08 12:53:19 -0800

    added base main and homepage handlebars

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+// const router = require('express').Router()
+
+// const homeRoutes = require('./homepage-routes');
+
+// router.use('/', homeRoutes);
+
+// module.exports = router;
---
+++ controllers/homepage-routes.js
@@ -0,0 +1,9 @@
+const router =require('express').Router();
+
+router.get('/', (req, res) => {
+    try{
+        res.render('homepage');
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
