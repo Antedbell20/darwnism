786c6fffb7f4bd3f8b5a003407810fa7a84cac6b
Andrew Hall <andrewchall92@gmail.com>
2023-11-08 15:36:52 -0800

    Included animal for user in the get request

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Animal, Score } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
@@ -18,7 +18,14 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const userData = await User.findAll(req.body);
+        const userData = await User.findAll({
+            include: [
+                {
+                    model: Animal,
+                    attributes: ['name']
+                }
+            ]
+        });
         res.status(200).json(userData);
     } catch (error) {
         res.status(400).json(error);
