d72a0f9dd9a4a5c19dc87da0f2d08b123adc0631
Ian Sills <iansills04@gmail.com>
2023-11-08 22:43:07 -0600

    some login functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -26,24 +26,9 @@ router.get('/', async (req, res) => {
 });
 
 // added by Ian
-router.post('/', async (req, res) => {
-    console.log(req.body);
+ router.post('/login', async (req, res) => {
     try {
-      const userData = await User.create(req.body);
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.loggedIn = true;
-  
-        res.status(200).json(userData);
-      });
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
+      const userData = await User.findOne({ where: { username: req.body.username } });
   
       if (!userData) {
         res
--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -23,13 +23,12 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+   const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
+    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
