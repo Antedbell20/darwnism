7c04753ecdba4356eb99f3e64b094485574f2040
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-11-09 23:55:40 -0800

    playing with document construction on handlebars

--- public/js/questions.js
+++ public/js/questions.js
@@ -4,22 +4,33 @@ const answerButtonA = document.querySelector('#answerA');
 
 const questionObjArray = JSON.parse(retrieveQuestionString);
 
+function rearrangeAnswers(array){
+for (let i = array.length -1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i+1));
+    [array[i],array[j]] = [array[j],array[i]];
+}
+return array;
+}
+
 answerButtonA.addEventListener("click",function(){
     console.log(questionObjArray);
-     // console.log(questionObjArray);
+
      for (let i = 0; i < questionObjArray.length; i++) {
         const answerArray = [];
         
         answerArray.push(questionObjArray[i].correct_answer)
-        
-        // console.log("question: "+ questionObjArray[i].question);
-        // console.log("correct answer: "+ questionObjArray[i].correct_answer);
+
         for (let j = 0; j < questionObjArray[i].incorrect_answers.length; j++) {
             answerArray.push(questionObjArray[i].incorrect_answers[j])
-            // console.log("incorrect answers: "+ data.results[i].incorrect_answers[j]);
+
         }
-        // console.log('data res' + data.results[i].question)
-        console.log(answerArray)
+
+        shuffledAnswers = rearrangeAnswers(answerArray);
+        console.log(shuffledAnswers);
+        
     }
+
+    
 });
+
 
